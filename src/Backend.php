cfda6e43d0ef054862881839a9be238008ca44a8
Jean-Christian Denis <contact@jcdenis.fr>
2023-04-10 19:52:57 +0200

    use namespace

--- src/Backend.php
+++ src/Backend.php
@@ -5,26 +5,52 @@
  * @package Dotclear
  * @subpackage Plugin
  *
- * @author Tomtom (http://blog.zenstyle.fr/)
+ * @author Tomtom and Contributors
  *
  * @copyright Jean-Christian Denis
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-if (!defined('DC_CONTEXT_ADMIN')) {
-    return null;
-}
+declare(strict_types=1);
 
-dcCore::app()->addBehavior('adminPageHTMLHead', function () {
-    echo
-    dcPage::jsLoad(dcPage::getPF('commentNotifications/_admin.js')) .
-    '<script type="text/javascript">' . "\n" .
-    "//<![CDATA[\n" .
-    dcPage::jsVar('notificator.nb_comments', dcCore::app()->blog->getComments([], true)->f(0)) .
-    dcPage::jsVar('notificator.reload_nb_comments', (preg_match('/' . preg_quote(dcCore::app()->adminurl->get('admin.comments')) . '(.*)?/', $_SERVER['REQUEST_URI']) ? 'true' : 'false')) .
-    dcPage::jsVar('notificator.msg.comment', __('comment')) .
-    dcPage::jsVar('notificator.msg.comments', __('comments')) .
-    dcPage::jsVar('notificator.msg.recent', __('new')) .
-    dcPage::jsVar('notificator.msg.recents', __('news')) .
-    "\n//]]>\n" .
-    "</script>\n";
-});
+namespace Dotclear\Plugin\commentNotifications;
+
+use dcCore;
+use dcNsProcess;
+use dcPage;
+
+class Backend extends dcNsProcess
+{
+    public static function init(): bool
+    {
+        static::$init = defined('DC_CONTEXT_ADMIN')
+            && dcCore::app()->auth->check(dcCore::app()->auth->makePermissions([
+                dcCore::app()->auth::PERMISSION_USAGE,
+            ]), dcCore::app()->blog->id);
+
+        return static::$init;
+    }
+
+    public static function process(): bool
+    {
+        if (!static::$init) {
+            return false;
+        }
+
+        dcCore::app()->addBehavior('adminPageHTMLHead', function (): void {
+            echo
+            dcPage::jsModuleLoad(My::id() . '/js/backend.js') .
+            '<script type="text/javascript">' . "\n" .
+            "//<![CDATA[\n" .
+            dcPage::jsVar('notificator.nb_comments', dcCore::app()->blog->getComments([], true)->f(0)) .
+            dcPage::jsVar('notificator.reload_nb_comments', (preg_match('/' . preg_quote(dcCore::app()->adminurl->get('admin.comments')) . '(.*)?/', $_SERVER['REQUEST_URI']) ? 'true' : 'false')) .
+            dcPage::jsVar('notificator.msg.comment', __('comment')) .
+            dcPage::jsVar('notificator.msg.comments', __('comments')) .
+            dcPage::jsVar('notificator.msg.recent', __('new')) .
+            dcPage::jsVar('notificator.msg.recents', __('news')) .
+            "\n//]]>\n" .
+            "</script>\n";
+        });
+
+        return true;
+    }
+}
--- src/Prepend.php
+++ src/Prepend.php
@@ -5,15 +5,41 @@
  * @package Dotclear
  * @subpackage Plugin
  *
- * @author Tomtom (http://blog.zenstyle.fr/)
+ * @author Tomtom and Contributors
  *
  * @copyright Jean-Christian Denis
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-if (!defined('DC_RC_PATH')) {
-    return null;
+declare(strict_types=1);
+
+namespace Dotclear\Plugin\commentNotifications;
+
+use dcCore;
+use dcNsProcess;
+use Dotclear\Helper\Html\XmlTag;
+
+class Prepend extends dcNsProcess
+{
+    public static function init(): bool
+    {
+        static::$init = defined('DC_RC_PATH');
+
+        return static::$init;
+    }
+
+    public static function process(): bool
+    {
+        if (!static::$init) {
+            return false;
+        }
+
+        dcCore::app()->rest->addFunction('getNbComments', function (): XmlTag {
+            $rsp = new XmlTag();
+            $rsp->comments(dcCore::app()->blog->getComments(['no_content' => true], true)->f(0));
+
+            return $rsp;
+        });
+
+        return true;
+    }
 }
-
-Clearbricks::lib()->autoload(['commentNotificationsRestMethods' => __DIR__ . '/_services.php']);
-
-dcCore::app()->rest->addFunction('getNbComments', ['commentNotificationsRestMethods','getNbComments']);
